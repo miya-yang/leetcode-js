65a5e8aa654c196d4ae755f4514da469e8471c48
Miya_yang <wow511287680@163.com>
2018-03-24 01:22:39 +0800

    修正了001注释语义的错误

--- Code/leetcode-001-Two Sum.js
+++ Code/leetcode-001-Two Sum.js
@@ -4,7 +4,7 @@
  * @return {number[]}
  */
 
-// 提交通过版本
+// 初次提交版本
 var twoSum = function(nums, target) {
     for(var i = 0;i < nums.length;i++) {
         for(var j = parseInt(i+1);j < nums.length;j++) {
@@ -19,7 +19,7 @@ var twoSum = function(nums, target) {
 var twoSum2 = function (nums, target) {
     var temp = {};
     for (var i = 0; i < nums.length; i++) {
-        // 临时变量中不存在以该值或其对象值为key的对象
+        // 临时变量中不存在以该配对值为key的对象
         if (!(target - nums[i] in temp)) {
             temp[nums[i]] = i;
         } else {
