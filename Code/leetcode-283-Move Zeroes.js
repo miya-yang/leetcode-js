87378a29b7dda4fa51505188a89a67d0216da646
Miya_yang <wow511287680@163.com>
2018-03-25 01:19:54 +0800

    提交283

---
+++ Code/leetcode-283-Move Zeroes.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+	var i = 0,
+		j = 0;
+	do {
+		if (nums[i] === 0) {
+			nums.splice(i, 1);
+			nums.push(0);
+		} else {
+			i++;
+		}
+		j++;
+	} while (j < nums.length);
+};
