9262cce09665f4f660b41113f2ed3ebd39f8b3df
Miya_yang <wow511287680@163.com>
2018-04-24 22:38:10 +0800

    提交561

---
+++ Code/leetcode-561-Array Partition I.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arrayPairSum = function(nums) {
+    var total = 0;
+    nums.sort(function(v1, v2) {
+        return v1 - v2;
+    });
+    for(var i = 0;i < nums.length;i += 2) {
+        total += nums[i];
+    }
+    return total;
+};
