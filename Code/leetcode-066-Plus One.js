adebb1d5b292351a015dde2e546474340eacf6e2
Miya_yang <wow511287680@163.com>
2018-07-31 11:36:49 +0800

    提交 066

---
+++ Code/leetcode-066-Plus One.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function (digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      digits[i]++
+      return digits
+    }
+    digits[i] = 0
+  }
+  return [1, ...digits]
+}
