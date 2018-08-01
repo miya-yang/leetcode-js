de4c78de839ff742f6a998f914aaf475264f3f59
Miya_yang <wow511287680@163.com>
2018-08-01 17:42:54 +0800

    优化 338

--- Code/leetcode-338-Counting Bits.js
+++ Code/leetcode-338-Counting Bits.js
@@ -4,8 +4,9 @@
  */
 var countBits = function (num) {
   let arr = []
-  for (let i = 0; i <= num; i++) {
-    arr.push(Number(i).toString(2).match(/1/g) === null ? 0 : Number(i).toString(2).match(/1/g).length)
+  arr[0] = 0
+  for (let i = 1; i <= num; i++) {
+    arr.push(Number(i).toString(2).match(/1/g).length)
   }
   return arr
 }
