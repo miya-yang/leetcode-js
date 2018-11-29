cf34d40ccf5dbaf85d81ed02be4694e6f350bfe5
miyang xu <xumiyang@interns.chuangxin.com>
2018-11-29 11:13:17 +0800

    更新 009

--- Code/leetcode-009-Palindrome Number.js
+++ Code/leetcode-009-Palindrome Number.js
@@ -12,3 +12,14 @@ var isPalindrome = function (x) {
   }
   return true
 }
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+  if (x < 0) {
+    return false
+  }
+  return x.toString() === (x.toString().split('').reverse().join(''))
+}
