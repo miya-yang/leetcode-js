f00940e5c501eb6eb131507f057359e941b43c83
xumiyang <xumiyang@58.com>
2019-12-20 14:59:43 +0800

    优化 041

--- Code/leetcode-041-First Missing Positive.js
+++ Code/leetcode-041-First Missing Positive.js
@@ -4,12 +4,21 @@
  */
 var firstMissingPositive = function (nums) {
   let arr = nums.filter(item => item > 0)
-  arr.sort((a, b) => a - b)
-  if (arr[0] !== 1) {
+  if (arr.length === 0) {
     return 1
   }
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > arr[i - 1] + 1) {
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i
+    for (let j = i; j < arr.length; j++) {
+      if (arr[minIndex] > arr[j]) {
+        minIndex = j
+      }
+    }
+    [arr[minIndex], arr[i]] = [arr[i], arr[minIndex]]
+    if (i === 0 && arr[0] !== 1) {
+      return 1
+    }
+    if (i >= 1 && arr[i] > arr[i - 1] + 1) {
       return arr[i - 1] + 1
     }
   }
