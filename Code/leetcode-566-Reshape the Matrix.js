1a18a366555a1c3b7b17b1a0cedd0e7f76b7a4ed
Miyang <wow511287680@163.com>
2021-02-17 17:17:25 +0800

    add 566

---
+++ Code/leetcode-566-Reshape the Matrix.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[][]} nums
+ * @param {number} r
+ * @param {number} c
+ * @return {number[][]}
+ */
+var matrixReshape = function(nums, r, c) {
+    if (nums.length === 0 || (nums[0] && nums[0].length === 0)) {
+        return nums;
+    }
+    if (nums.length * nums[0].length !== r * c) {
+        return nums;
+    }
+    const arr = [];
+    const numsArr = nums.flat();
+    let index = 0;
+    for (let i = 0; i < r; i++) {
+        arr[i] = numsArr.slice(index, index + c);
+        index += c;
+    }
+    return arr;
+};
