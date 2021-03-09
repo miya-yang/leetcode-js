fe6ce0a7b1557b3c83fb9affa96805612ee5e52c
Miyang <wow511287680@163.com>
2021-03-09 20:55:58 +0800

    add 226

---
+++ Code/leetcode-226-Invert Binary Tree.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=javascript
+ *
+ * [226] 翻转二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    if (root == null) {
+        return root;
+    }
+    [root.left, root.right] = [root.right, root.left];
+    invertTree(root.left);
+    invertTree(root.right);
+
+    return root;
+};
+// @lc code=end
+
