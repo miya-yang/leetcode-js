5b16db9a73625d0fc4962defd5e3cbfe5c03ed32
Miyang <wow511287680@163.com>
2021-03-14 00:24:26 +0800

    update 46

--- Code/leetcode-046-Permutations.js
+++ Code/leetcode-046-Permutations.js
@@ -3,38 +3,21 @@
  * @return {number[][]}
  */
 var permute = function(nums) {
-    // let result = []
-    // let perm = (c, a) => {
-    //     if (a.length === 1) {
-    //         c.push(a[0])
-    //         result.push(c)
-    //     } else {
-    //         for (let i = 0; i < a.length; i++) {
-    //             let curr = c.slice()
-    //             let data = a.slice()
-    //             curr.push(data[i])
-    //             data.splice(i, 1)
-    //             perm(curr, data)
-    //         }
-    //     }
-    // }
-    // perm([], nums)
-    // return result
+    let result = [];
 
-    let result = []
-    let perm = (c, p, q) => {
-        if (p === q) {
-            result.push(c)
-        } else {
-            for (let i = p; i < q; i++) {
-                let curr = c.slice()
-                let temp = curr[i]
-                curr[i] = curr[p]
-                curr[p] = temp
-                perm(curr, p + 1, q)
-            }
+    const backtracking = (arr, remain) => {
+        if (remain.length === 0) {
+            result.push(arr);
+        }
+        for (let i = 0; i < remain.length; i++) {
+            let arrClone = arr.slice();
+            arrClone.push(remain[i]);
+            let remainClone = remain.slice();
+            remainClone.splice(i, 1);
+            backtracking(arrClone, remainClone);
         }
     }
-    perm(nums, 0, nums.length)
-    return result
+
+    backtracking([], nums);
+    return result;
 };
