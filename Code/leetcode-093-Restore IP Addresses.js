088712d45eac3c312d01b1b902266ea72198de21
Miyang <wow511287680@163.com>
2021-03-13 14:10:56 +0800

    update 93

--- Code/leetcode-093-Restore IP Addresses.js
+++ Code/leetcode-093-Restore IP Addresses.js
@@ -3,30 +3,33 @@
  * @return {string[]}
  */
 var restoreIpAddresses = function (s) {
-  let ips = []
-  let ra = (arr, str) => {
-    let len = arr.length
-    // 判断剩余数据是否可以组成IP地址
-    if (str.length < 4 - len || str.length > 3 * (4 - len)) {
-      return false
-    }
-    // 结束条件
-    if (arr.length === 4 && arr.join('') === s) {
-      ips.push(arr.join('.'))
-    } else {
-      // 进行尝试取值
-      let strArr = str.split('')
-      for (let i = 0, minLen = Math.min(3, str.length), ip; i < minLen; i++) {
-        ip = str.substr(0, i + 1)
-        if (i > 0 && Number(ip.substr(0, 1)) === 0) {
-          break
-        }
-        if (Number(ip) < 256) {
-          ra(arr.concat([ip]), str.substr(i + 1))
-        }
-      }
-    }
+  let result = [];
+  if(s.length < 4 || s.length > 16) {
+      return result;
   }
-  ra([], s)
-  return ips
+  const backtracking = (arr, startAt) => {
+      if ((4 - arr.length) * 3 < s.length - startAt) {
+          return;
+      }
+      if (arr.length === 4) {
+          if (startAt >= s.length) {
+              result.push(arr.join('.'));
+          }
+          return;
+      }
+      for (let i = startAt + 1; i < startAt + 4 && i <= s.length; i++) {
+          let num = s.substring(startAt, i);
+          if (num <= 255) {
+              let tempArr = arr.slice();
+              tempArr.push(num);
+              backtracking(tempArr, i);
+          }
+          if (num == 0 || num > 255) {
+              break;
+          }
+      }
+  }
+
+  backtracking([], 0);
+  return result;
 }
