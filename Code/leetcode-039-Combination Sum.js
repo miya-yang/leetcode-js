0d21e8fca97243d2dc998e27f9f44a17179e8366
miyang Xu <wow511287680@163.com>
2021-08-03 16:57:22 +0800

    update 039

--- Code/leetcode-039-Combination Sum.js
+++ Code/leetcode-039-Combination Sum.js
@@ -10,27 +10,51 @@
  * @param {number} target
  * @return {number[][]}
  */
-var combinationSum = function(candidates, target) {
+ var combinationSum = function(candidates, target) {
     let result = [];
 
-    const backtracking = (candidates, arr, total, target, start) => {
-        if (total > target) {
-            return;
-        }
-        if (total === target) {
-            result.push(arr);
+    candidates.sort((a, b) => a - b);
+
+    const backtracking = (a, c, t) => {
+        if (t === 0) {
+            result.push(a);
             return;
         }
 
-        for (let i = start; i < candidates.length; i++) {
-            let tempArr = arr.slice();
-            tempArr.push(candidates[i]);
-            backtracking(candidates, tempArr, total + candidates[i], target, i);
+        const len = c.length;
+
+        for (let i = 0; i < len; i++) {
+            if (t >= c[i]) {
+                backtracking([...a, c[i]], c.slice(i), t - c[i]);
+            }
         }
     }
 
-    backtracking(candidates, [], 0, target, 0);
+    backtracking([], candidates, target);
+
     return result;
 };
+// var combinationSum = function(candidates, target) {
+//     let result = [];
+
+//     const backtracking = (candidates, arr, total, target, start) => {
+//         if (total > target) {
+//             return;
+//         }
+//         if (total === target) {
+//             result.push(arr);
+//             return;
+//         }
+
+//         for (let i = start; i < candidates.length; i++) {
+//             let tempArr = arr.slice();
+//             tempArr.push(candidates[i]);
+//             backtracking(candidates, tempArr, total + candidates[i], target, i);
+//         }
+//     }
+
+//     backtracking(candidates, [], 0, target, 0);
+//     return result;
+// };
 // @lc code=end
 
