b7a3c6b473d7f1f6354dcea168897a4923ad60c6
miyang Xu <wow511287680@163.com>
2021-08-09 18:56:48 +0800

    update 122

--- Code/leetcode-122-Best Time to Buy and Sell Stock II.js
+++ Code/leetcode-122-Best Time to Buy and Sell Stock II.js
@@ -17,3 +17,23 @@ var maxProfit = function (prices) {
   }
   return count
 }
+
+// 解法二
+var maxProfit2 = function(prices) {
+  let result = 0;
+  let max = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+      if (prices[i] < prices[i - 1]) {
+          result += max;
+          max = 0;
+          continue;
+      }
+
+      max += prices[i] - prices[i - 1];
+  }
+
+  result += max;
+
+  return result;
+}
